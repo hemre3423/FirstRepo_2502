5b2d1ac664da3314c30541a3a2fb808d081b48ae
Hüseyin Emre <huseyinemre45251@outlook.com>
2025-02-25 15:07:00 +0300

    Form load mesaj kutusu eklendi

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Ytb2GitHubTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
